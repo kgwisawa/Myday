708014d4f99da27a02ffdcc11c4fab9ac5562e39
wisawa.c <wisawa.cho@ku.th>
2022-08-06 02:14:00 +0700

    [ Start ] - setup project

---
+++ Myday/Myday/MydayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MydayApp.swift
+//  Myday
+//
+//  Created by วิศวะ โชติพันธุ์พงศ์ on 6/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct MydayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
